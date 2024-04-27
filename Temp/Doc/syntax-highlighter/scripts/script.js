804639477b5d39b89b42d37827defa7ea24c2f04
bhoomish-v-a <157049331+bhoomish-v-a@users.noreply.github.com>
2024-04-27 14:44:15 +0530

    Update script.js

--- Temp/Doc/syntax-highlighter/scripts/script.js
+++ Temp/Doc/syntax-highlighter/scripts/script.js
@@ -16,3 +16,20 @@ document.getElementById('emailForm').addEventListener('submit', function(event) 
             alert('Email sending failed. Please try again.');
         });
 });
+
+document.getElementById('emailForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const form = this; // Reference to the form element
+
+    // Replace 'service_sfvli6h', 'template_ttemf9t' with your actual EmailJS service and template IDs
+    emailjs.sendForm('service_sfvli6h', 'template_pw0o46c', form)
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('Email sent successfully!');
+            form.reset(); // Reset the form after successful submission
+        }, function(error) {
+            console.log('FAILED...', error);
+            alert('Email sending failed. Please try again.');
+        });
+});
