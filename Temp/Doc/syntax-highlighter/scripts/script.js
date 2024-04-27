9f162961e41d65861ebcf9cdc0c22924a718052a
bhoomish-v-a <157049331+bhoomish-v-a@users.noreply.github.com>
2024-04-28 00:07:56 +0530

    Update script.js

--- Temp/Doc/syntax-highlighter/scripts/script.js
+++ Temp/Doc/syntax-highlighter/scripts/script.js
@@ -9,13 +9,13 @@ document.getElementById('emailForm').addEventListener('submit', function(event) 
     emailjs.sendForm('service_sfvli6h', 'template_ttemf9t', form)
         .then(function(response) {
             console.log('SUCCESS!', response.status, response.text);
+         
             form.reset(); // Reset the form after successful submission
         }, function(error) {
             console.log('FAILED...', error);
             alert('Email sending failed. Please try again.');
         });
 });
-
 document.getElementById('emailForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
@@ -32,3 +32,26 @@ document.getElementById('emailForm').addEventListener('submit', function(event) 
             alert('Email sending failed. Please try again.');
         });
 });
+
+function validate(){
+    var text = document.getElementById('mail').value;
+    console.log('Input Value:', text);
+    var regx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    console.log('Regex Test Result:', regx.test(text));
+    if(text==''){
+        document.getElementById('text2').style.visibility = 'hidden';
+
+    }
+    else{
+
+        if(regx.test(text)){
+        document.getElementById('text2').innerHTML = "Valid";
+        document.getElementById('text2').style.visibility = 'visible';
+        document.getElementById('text2').style.color = 'green';
+    } else {
+        document.getElementById('text2').innerHTML = "Invalid";
+        document.getElementById('text2').style.visibility = 'visible';
+        document.getElementById('text2').style.color = 'red';
+    }
+    }
+}
